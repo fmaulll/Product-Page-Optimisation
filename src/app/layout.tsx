1bec2ed9d58c72c58ab2b26f13924b2197a33211
fmaulll <90718125+fmaulll@users.noreply.github.com>
2025-10-22 22:40:37 +0700

    use the snow component in layout so it sta server comp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Snowfall from "@/components/Snowfall";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Snowfall />
         {children}
       </body>
     </html>
